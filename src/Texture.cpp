6bfe87f7286e7924f5101de12fe6429d44fead4f
Jared Mulconry <jared.mulconry@ad.aie.edu>
2016-06-23 17:58:52 +1000

    Actually added Texture.cpp

---
+++ src/Texture.cpp
@@ -0,0 +1,42 @@
+#include "Texture.hpp"
+#include "GLFW\glfw3.h"
+
+namespace GlProj
+{
+	namespace Graphics
+	{
+		Texture::Texture(Texture&& o) noexcept
+			: textureHandle(o.textureHandle)
+		{
+			o.textureHandle = invalidHandle;
+		}
+		Texture& Texture::operator=(Texture&& o) noexcept
+		{
+			if (&o != this)
+			{
+				if (textureHandle != invalidHandle)
+				{
+					glDeleteTextures(1, &textureHandle);
+				}
+				textureHandle = o.textureHandle;
+				o.textureHandle = invalidHandle;
+			}
+			return *this;
+		}
+		Texture::~Texture()
+		{
+			if (textureHandle != invalidHandle)
+			{
+				glDeleteTextures(1, &textureHandle);
+			}
+		}
+		Texture::Texture(GLuint handle) noexcept
+			: textureHandle(handle)
+		{
+		}
+		GLuint Texture::GetHandle() const noexcept
+		{
+			return textureHandle;
+		}
+	}
+}

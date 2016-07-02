1b64e8f41ef90950b65cc6d530188c4c5d5308ba
Jared Mulconry <jaredmulconry@gmail.com>
2016-07-03 00:19:34 +1000

    Added a Mesh class.

---
+++ src/include/Mesh.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "OpenGLTypes.hpp"
+#include "MeshDataBuffer.hpp"
+#include <vector>
+
+namespace GlProj
+{
+	namespace Graphics
+	{
+		class Mesh
+		{
+			std::vector<MeshDataBuffer> data;
+
+		public:
+
+		};
+	}
+}

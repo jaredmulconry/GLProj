3bf1b790d7cf595755aea27feef955715f75476c
Jared Mulconry <jaredmulconry@gmail.com>
2016-08-10 17:37:03 +1000

    Tried and failed to make a constructor that takes an assimp scene. Requires abstracting out.

--- src/include/Model.hpp
+++ src/include/Model.hpp
@@ -1,18 +1,23 @@
 #pragma once
+#include "Material.hpp"
 #include <memory>
 #include <vector>
+
+struct aiScene;
 
 namespace GlProj
 {
 	namespace Graphics
 	{
 		class Mesh;
-		class Material;
 
 		class Model
 		{
 			std::vector<std::shared_ptr<Mesh>> meshes;
-			std::vector<std::shared_ptr<Material>> materials;
+			std::vector<Material> materials;
+		public:
+			Model() = default;
+			Model(const std::vector<std::shared_ptr<Mesh>>&, const std::vector<Material>&);
 		};
 	}
 }

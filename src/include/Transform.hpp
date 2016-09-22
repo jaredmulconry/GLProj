9c04ad7f17b2442eb60cc02d2d76aa9051be57a0
Jared Mulconry <jaredmulconry@gmail.com>
2016-09-22 14:01:17 +1000

    Removed more backslashes from file paths. Darn Windows.

--- src/include/Transform.hpp
+++ src/include/Transform.hpp
@@ -1,7 +1,7 @@
 #pragma once
-#include "glm\fwd.hpp"
-#include "glm\vec3.hpp"
-#include "glm\gtc\quaternion.hpp"
+#include "glm/fwd.hpp"
+#include "glm/vec3.hpp"
+#include "glm/gtc/quaternion.hpp"
 
 namespace GlProj
 {

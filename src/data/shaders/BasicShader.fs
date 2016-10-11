2bbacde91d7bcdf4c601e9ad52f4f45936c6bfcb
Jared Mulconry <jaredmulconry@gmail.com>
2016-10-11 16:49:56 +1100

    Removed the texture test from the basic shader. It's clear that some solution to deal with arbitrary bindings to materials is needed, something that doesn't depend on the owner doing the binding themselves.

--- src/data/shaders/BasicShader.fs
+++ src/data/shaders/BasicShader.fs
@@ -2,12 +2,9 @@
 
 in vec4 f_position;
 
-uniform sampler2D magicalTexture;
-
 out vec4 f_colour;
 
 void main()
 {
-	f_colour = texture(magicalTexture, f_position.xy);
-	//f_colour = vec4(f_position.xyz, 1.0);
+	f_colour = vec4(f_position.xyz, 1.0);
 }
